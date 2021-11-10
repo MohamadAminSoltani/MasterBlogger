51f4658ff0997110c0a5bdf0639b0ca3f549833f
M.A.Soltani <maxmadmas@gmail.com>
2021-11-10 18:25:19 +0330

    UI Added

--- MB.Presentation.MVCCore/Program.cs
+++ MB.Presentation.MVCCore/Program.cs
@@ -1,6 +1,10 @@
+using MB.Infrastructure.Core;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+Bootstrapper.Config(builder.Services, builder.Configuration.GetConnectionString("MasterBloggerDB"));
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -9,7 +13,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -21,5 +24,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute()); 
 
 app.Run();
